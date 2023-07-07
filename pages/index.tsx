4231722f8dcd55d5c6cc57c66bde92f240e3d539
Albertino Augusto <albertinoaugusto.it@outlook.com>
2023-07-07 21:37:42 +0200

    Files deleted

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,11 +12,9 @@ type Props = {
   // Add custom props here
 }
 
-const Homepage = (
-  _props: InferGetStaticPropsType<typeof getStaticProps>
-) => {
+const Homepage = (_props: InferGetStaticPropsType<typeof getStaticProps>) => {
   const router = useRouter()
-  const { t, i18n } = useTranslation('common')
+  const { t, i18n } = useTranslation('main')
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const onToggleLanguageClick = (newLocale: string) => {
@@ -35,96 +33,26 @@ const Homepage = (
   return (
     <>
       <main>
-        <Header heading={t('h1')} title={t('title')} />
-        <div style={{ display: 'inline-flex', width: '90%' }}>
-          <div style={{ width: '33%' }}>
-            <h3 style={{ minHeight: 70 }}>
-              {t('blog.appDir.question')}
-            </h3>
-            <p>
-              <Trans i18nKey="blog.appDir.answer">
-                Then check out
-                <a href={t('blog.appDir.link')}>this blog post</a>
-                .
-              </Trans>
-            </p>
-            <a href={t('blog.appDir.link')}>
-              <img
-                style={{ width: '50%' }}
-                src="https://locize.com/blog/next-13-app-dir-i18n/next-13-app-dir-i18n.jpg"
-              />
-            </a>
-          </div>
-          <div style={{ width: '33%' }}>
-            <h3 style={{ minHeight: 70 }}>
-              {t('blog.optimized.question')}
-            </h3>
-            <p>
-              <Trans i18nKey="blog.optimized.answer">
-                Then you may have a look at
-                <a href={t('blog.optimized.link')}>this blog post</a>
-                .
-              </Trans>
-            </p>
-            <a href={t('blog.optimized.link')}>
-              <img
-                style={{ width: '50%' }}
-                src="https://locize.com/blog/next-i18next/next-i18next.jpg"
-              />
-            </a>
-          </div>
-          <div style={{ width: '33%' }}>
-            <h3 style={{ minHeight: 70 }}>
-              {t('blog.ssg.question')}
-            </h3>
-            <p>
-              <Trans i18nKey="blog.ssg.answer">
-                Then you may have a look at
-                <a href={t('blog.ssg.link')}>this blog post</a>.
-              </Trans>
-            </p>
-            <a href={t('blog.ssg.link')}>
-              <img
-                style={{ width: '50%' }}
-                src="https://locize.com/blog/next-i18n-static/title.jpg"
-              />
-            </a>
-          </div>
-        </div>
-        <hr style={{ marginTop: 20, width: '90%' }} />
-        <div>
-          <Link href="/" locale={changeTo}>
-            <button>{t('change-locale', { changeTo })}</button>
-          </Link>
-          {/* alternative language change without using Link component
-          <button onClick={() => onToggleLanguageClick(changeTo)}>
-            {t('change-locale', { changeTo })}
-          </button>
-          */}
-          {/* alternative language change without using Link component, but this will change language only on client side
-          <button onClick={() => clientSideLanguageChange(changeTo)}>
-            {t('change-locale', { changeTo })}
-          </button> */}
-          <Link href="/second-page">
-            <button type="button">{t('to-second-page')}</button>
-          </Link>
-        </div>
+        <p>{t('WELCOME')}</p>
+        {/* <Trans i18nKey="helpLocize" t={t}>
+          With using
+          <a href="https://locize.com" target="_new">
+            locize
+          </a>
+          you directly support the future of
+          <a href="https://www.i18next.com" target="_new">
+            i18next
+          </a>
+          .
+        </Trans> */}
       </main>
-      <Footer />
     </>
   )
 }
 
 // or getServerSideProps: GetServerSideProps<Props> = async ({ locale })
-export const getStaticProps: GetStaticProps<Props> = async ({
-  locale,
-}) => ({
-  props: {
-    ...(await serverSideTranslations(locale ?? 'en', [
-      'common',
-      'footer',
-    ])),
-  },
+export const getStaticProps: GetStaticProps<Props> = async ({ locale }) => ({
+  props: {...(await serverSideTranslations(locale ?? 'en', ['main']))} // You add more file names on this array
 })
 
 export default Homepage
